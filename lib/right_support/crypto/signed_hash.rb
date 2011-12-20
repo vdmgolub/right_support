e41cfefa47d251e395af510b29dff93a3759e54e
Vadim Golub <vdm.golub@gmail.com>
2011-12-20 21:45:59 +0300

    Correct encoder type added
    Change-Id: I752289f268968e72c783fc0984c02cb0136691b1

--- lib/right_support/crypto/signed_hash.rb
+++ lib/right_support/crypto/signed_hash.rb
@@ -4,7 +4,7 @@ module RightSupport::Crypto
   class SignedHash
     DEFAULT_OPTIONS = {
       :digest   => Digest::SHA1,
-      :encoding => YAML
+      :encoding => JSON
     }
 
     def initialize(hash={}, options={})
